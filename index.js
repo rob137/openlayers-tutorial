d5379c6a1b24ef5c17592bcaa1d1440fbae09003
rob <robertaxelkirby@gmail.com>
2019-01-03 17:17:46 +0000

    Complete second tutorial

--- index.js
+++ index.js
@@ -5,13 +5,9 @@ import OSM from 'ol/source/OSM';
 
 const map = new Map({
   target: 'map',
-  layers: [
-    new TileLayer({
-      source: new OSM(),
-    }),
-  ],
+  layers: [ new TileLayer({ source: new OSM() })],
   view: new View({
     center: [0, 0],
-    zoom: 0,
+    zoom: 6,
   }),
-})
+});
